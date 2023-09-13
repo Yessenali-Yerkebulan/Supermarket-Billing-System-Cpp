5209b90402ad8ebedcdf3a9b43a202a606c97ce8
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-13 18:24:57 +0600

    add edit, remove product method

--- main.cpp
+++ main.cpp
@@ -181,3 +181,93 @@ void shopping::add() {
 
 	cout << "\n\n\t\t Record inserted!";
 }
+
+
+void shopping::edit()
+{
+	fstream data, data1;
+	int pkey;
+	int token = 0;
+	int c;
+	float p;
+	float d;
+	string n;
+	cout << "\n\t\t\t Modify the record";
+	cout << "\n\t\t\t Product code: ";
+	cin >> pkey;
+	data.open("database.txt", ios::in);
+
+	if (!data) {
+		cout << "\n\nFile doesn't exist! ";
+	}
+	else {
+		data1.open("database1.txt", ios::app | ios::out);
+
+		data >> pcode >> pname >> price >> dis;
+		while (!data.eof()) {
+			if (pkey == pcode) {
+				cout << "\n\t\t\t Product new code: ";
+				cin >> c;
+				cout << "\n\t\t\t Name of the product: ";
+				cin >> n;
+				cout << "\n\t\t Price: ";
+				cin >> p;
+				cout << "\n\t\t Discount: ";
+				cin >> d;
+				data1 << " " << c << " " << n << " " << p << " " << d << "\n";
+				cout << "\n\n\t\t Record edited ";
+				token++;
+			}
+			else {
+				data1 << " " << pcode << " " << pname << " " << price << " " << dis << "\n";
+			}
+			data >> pcode >> pname >> price >> dis;
+		}
+
+		data.close();
+		data1.close();
+
+		remove("database.txt");
+		rename("database1.txt", "database.txt");
+
+		if (token == 0) {
+			cout << "\n\n Record not found sorry!";
+		}
+	}
+}
+
+void shopping::rem()
+{
+	fstream data, data1;
+	int pkey;
+	int token = 0;
+	cout << "\n\n\t Delete product";
+	cout << "\n\n\t Product code: ";
+	cin >> pkey;
+	data.open("database.txt", ios::in);
+	if (!data) {
+		cout << "File doesn't exist";
+	}
+
+	else {
+		data1.open("database1.txt", ios::app | ios::out);
+		data >> pcode >> pname >> price >> dis;
+		while (!data.eof()) {
+			if (pcode == pkey) {
+				cout << "\n\n\t Product deleted successfully";
+			}
+			else {
+				data1 << " " << pcode << " " << pname << " " << price << " " << dis << "\n";
+			}
+			data >> pcode >> pname >> price >> dis;
+		}
+		data.close();
+		data1.close();
+		remove("database.txt");
+		rename("database1.txt", "database.txt");
+
+		if (token == 0) {
+			cout << "\n\n Record not found";
+		}
+	}
+}
