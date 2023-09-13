0bec9e2d78a172399013cec060385dda63b03733
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-13 19:38:33 +0600

    add product list method

--- main.cpp
+++ main.cpp
@@ -271,3 +271,18 @@ void shopping::rem()
 		}
 	}
 }
+
+void shopping::list()
+{
+	fstream data;
+	data.open("database.txt", ios::in);
+	cout << "\n\n|_______________________________________________\n";
+	cout << "ProNo\t\tName\t\tPrice\n";
+	cout << "\n\n|________________________________________________\n";
+	data >> pcode >> pname >> price >> dis;
+	while (!data.eof()) {
+		cout << pcode << "\t\t" << pname << "\t\t" << price << "\n";
+		data >> pcode >> pname >> price >> dis;
+	}
+	data.close();
+}
